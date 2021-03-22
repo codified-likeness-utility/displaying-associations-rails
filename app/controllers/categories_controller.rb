74f1bc670df3271ac93ec60dfd2c467ada2c4b74
Ian Davies <mr.daviesian@gmail.com>
2021-03-22 15:50:38 -0500

    commit tree 1612e2a747785491821051025e9e00033acde6a3

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,5 @@
 class CategoriesController < ApplicationController
+
   def index
     @categories = Category.all
   end
@@ -31,4 +32,7 @@ class CategoriesController < ApplicationController
   def category_params
     params.require(:category).permit(:name)
   end
+
 end
+
+
